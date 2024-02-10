c15c11c88a2b12aee6189028654e8cce1a7d58a3
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2024-02-10 20:53:54 +0530

    feat: Complete footer, setup work system

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -2,5 +2,6 @@ import Competition from "./Competition";
 import GalleryItem from "./GalleryItem";
 import Sponsor from "./Sponsor";
 import TeamMember from "./TeamMember";
+import Work from "./Work";
 
-export const schemas = [GalleryItem, Sponsor, TeamMember, Competition];
+export const schemas = [GalleryItem, Sponsor, TeamMember, Competition, Work];
