501276f849d65acaf6e1ab1c73a497895a3e100d
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2024-02-11 23:34:55 +0530

    fix: Fix framer motion website overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
 	return (
-		<html className="scroll-smooth" lang="en">
-			<body className={`${inter.className} overflow-x-hidden`}>
+		<html className="overflow-x-hidden scroll-smooth" lang="en">
+			<body className={`${inter.className} relative overflow-x-hidden`}>
 				<AppProvider>{children}</AppProvider>
 			</body>
 		</html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import BgEffect from "@/components/BgEffect";
 import Footer from "@/components/Footer";
 import About from "@/components/Home/About";
 import Contact from "@/components/Home/Contact";
@@ -11,6 +12,7 @@ export default function Home(): React.JSX.Element {
 	return (
 		<>
 			<Navbar />
+			<BgEffect />
 			<div className="flex min-h-screen items-center justify-center">
 				<h1 className="text-5xl font-black">Hello World</h1>
 			</div>
